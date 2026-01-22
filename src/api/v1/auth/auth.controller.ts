c19f25cfcf40eec57840e6afcaacfcd499dabd87
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 16:35:07 +0300

    refactor: pass errors into the middleware instead of handling them inside the function

--- src/api/v1/auth/auth.controller.ts
+++ src/api/v1/auth/auth.controller.ts
@@ -5,27 +5,14 @@ import { BadRequestError } from "../../../errors/http-error"
 
 export  const authControllers = async (req: Request<{},AuthResponse, AuthRequest>, res:Response, next: NextFunction) =>{
     try{
-        const parsed = authRequestSchema.safeParse(req.body)
+        const parsed = authRequestSchema.parse(req.body)
        
-
-        if (!parsed.success) {
-            return res.status(400).json({
-                error: "Invalid request body",
-                details: parsed.error.issues.map(issue => ({
-                field: issue.path.join("."),
-                message: issue.message,
-            }))
-            });
-        }
-
-         const body = parsed.data
-
         
-        if (body.type === AuthRequestType.MAGIC_LINK){
-            return res.json(await authServices.authWithMagicLink(body))
+        if (parsed.type === AuthRequestType.MAGIC_LINK){
+            return res.json(await authServices.authWithMagicLink(parsed))
         }
-        else if(body.type === AuthRequestType.OAUTH){
-            return res.json(await authServices.authWithOAuth(body))
+        else if(parsed.type === AuthRequestType.OAUTH){
+            return res.json(await authServices.authWithOAuth(parsed))
         }
         else {
             throw new BadRequestError("Unsupported auth type");
