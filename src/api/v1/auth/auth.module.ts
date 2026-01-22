ae23fe2bfaa3f1790fcf712e66ea95e759207f17
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 19:29:30 +0300

    refactor

--- src/api/v1/auth/auth.module.ts
+++ src/api/v1/auth/auth.module.ts
@@ -1,5 +1,5 @@
 import { authControllers } from "./auth.controller";
-import authRouter from "./auth.route";
+import {authRouter} from "./auth.route";
 import { authServices } from "./auth.service"
 
 export { authControllers, authRouter, authServices };
