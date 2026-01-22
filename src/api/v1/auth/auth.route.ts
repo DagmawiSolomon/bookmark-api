389d4e3ab33c04b59de623d254b7c62d89a96106
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 19:30:08 +0300

    refactor: use modulerouter to define routes

--- src/api/v1/auth/auth.route.ts
+++ src/api/v1/auth/auth.route.ts
@@ -1,7 +1,14 @@
 import { Router } from "express";
 import { authControllers } from "./auth.controller";
+import { ModuleRouter } from "../../../shared/module-router";
 
 
-const authRouter = Router()
-authRouter.post('/auth', authControllers)
-export default  authRouter
+export const authRouter = new ModuleRouter('/auth')
+
+authRouter.post("/", authControllers.userAuthController);  
+authRouter.post("/magic/callback", authControllers.validateMagicLink)         
+// authRouter.get("/magic", authControllers.magicLink);      
+// authRouter.get("/oauth", authControllers.oauthInfo);      
+// authRouter.post("/oauth/:provider", authControllers.oauthLogin); 
+// authRouter.post("/refresh", authControllers.refresh);
+// authRouter.post("/logout", authControllers.logout);
