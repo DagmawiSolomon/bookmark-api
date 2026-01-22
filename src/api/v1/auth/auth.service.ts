97b38645b1bcfaef89c6be3286800ff2fa824fb6
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 19:11:23 +0300

    feat: implement a basic magiclink generation flow

--- src/api/v1/auth/auth.service.ts
+++ src/api/v1/auth/auth.service.ts
@@ -1,11 +1,25 @@
-import { AuthRequest,AuthResponse } from "./auth.schema"
+
+import { User } from "../../../models/user.model"
+import jwt from 'jsonwebtoken';
+import { AuthRequest,AuthRequestType,AuthResponse } from "./auth.schema"
+
+
+const JWT_SECRET = process.env.JWT_SECRET || ''
 
 const authWithMagicLink = async(req: AuthRequest):  Promise<AuthResponse>=>{
-    return {"token":"123", expiresIn:200}
+    const email = req.type === AuthRequestType.MAGIC_LINK ? req.email : undefined
+
+    const user = await User.findOne({email}) ?? await User.create({email})
+    const payload = { id: user._id, email: user.email }
+    const token = jwt.sign(payload, JWT_SECRET , { expiresIn: '15m' });
+    if(email){
+        console.log(`http://localhost:3000/auth/magic/callback?token=${token}`)
+    }
+    return {"token":token}
 } 
 
 const authWithOAuth = async(req:AuthRequest): Promise<AuthResponse>=>{
-     return {"token":"123", expiresIn:200}
+     return {"token":"123"}
 }
 
 export const authServices = {authWithMagicLink, authWithOAuth}
