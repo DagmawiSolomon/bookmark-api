c9cf5721c22370d18a655cf05ca750756801ce31
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 15:59:31 +0300

    feat: wire error handling middleware

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express from "express";
 import { v1Routers } from "./api/v1/v1.module";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
 const app = express();
 app.use(express.json());
 
 v1Routers.forEach(router => app.use("/api/v1", router));
+
+app.use(errorMiddleware)
 
 export default app
