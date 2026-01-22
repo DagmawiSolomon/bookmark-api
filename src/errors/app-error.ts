6550e92f98f276fc2169d8f85a26479621609125
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-22 16:35:36 +0300

    refactor: add error details to the class

--- src/errors/app-error.ts
+++ src/errors/app-error.ts
@@ -1,10 +1,18 @@
+export interface ErrorDetail{
+    path?: string,
+    message: string
+}
+
 export class AppError extends Error{
     public readonly statusCode: number
+    public readonly details?: ErrorDetail[]
     public readonly isOperational: boolean
+   
 
-    constructor(message: string, statusCode: number, isOperational = true){
+    constructor(message: string, statusCode: number,details?: ErrorDetail[], isOperational = true){
         super(message)
         this.statusCode = statusCode
+        this.details = details
         this.isOperational = isOperational
 
         Error.captureStackTrace(this, this.constructor);
