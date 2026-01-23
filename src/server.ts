bc7ed590115d37a6cb3a4fd7fb4329db53734ae7
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-23 16:29:32 +0300

    feat: import redis client

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,25 @@
 import dotenv from 'dotenv';
-dotenv.config()
+dotenv.config();
 
-import app from "./app";
-import { connectDB } from "./config/db";
+import app from './app';
+import { connectDB } from './config/mongodb';
+import { reidsClient } from './config/redis';
 
 const PORT = process.env.PORT || 3000;
 
-connectDB()
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+  try {
+    await connectDB();
+    await reidsClient.connect();
+    console.log('Redis connected');
 
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Failed to start server', err);
+    process.exit(1);
+  }
+}
+
+startServer();
