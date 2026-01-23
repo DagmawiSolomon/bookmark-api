8813977d432b5f44b54b855599fda59b6d5805be
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-23 05:05:12 +0300

    feat: define mongoodb shcema for magicLinks

---
+++ src/models/magicLink.model.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const magicLinkSchema = new mongoose.Schema({
+    userId: { type: mongoose.Types.ObjectId, ref: "User", required: true },
+    tokenHash: { type: String, required: true, index: true },
+    expiresAt: { type: Date, required: true },
+    usedAt: { type: Date, default: null },
+},
+{ timestamps: true }
+)
+
+magicLinkSchema.index(
+  { expiresAt: 1 },
+  { expireAfterSeconds: 0 }
+);
+
+
+export const MagicLink = mongoose.model("MagicLink", magicLinkSchema);
