76e89786f8afb05a99b9f9ae85c8b1d4ecdba4d9
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-28 16:57:52 +0300

    fix

--- src/api/v1/bookmarks/bookmarks.route.ts
+++ src/api/v1/bookmarks/bookmarks.route.ts
@@ -3,9 +3,9 @@ import { bookmarksControllers } from "./bookmarks.controller";
 
 const bookmarksRouter = new ModuleRouter("bookmarks/")
 
-bookmarksRouter.get("/", bookmarksControllers.getBookmarks())
-bookmarksRouter.post("/", bookmarksControllers.createBookmark())
-bookmarksRouter.get("/:id", bookmarksControllers.getBookmarksById())
-bookmarksRouter.put("/:id", bookmarksControllers.updateBookmark())
-bookmarksRouter.delete("/:id", bookmarksControllers.deleteBookmark())
+bookmarksRouter.get("/", bookmarksControllers.getBookmarks)
+bookmarksRouter.post("/", bookmarksControllers.createBookmark)
+bookmarksRouter.get("/:id", bookmarksControllers.getBookmarksById)
+bookmarksRouter.put("/:id", bookmarksControllers.updateBookmark)
+bookmarksRouter.delete("/:id", bookmarksControllers.deleteBookmark)
 
