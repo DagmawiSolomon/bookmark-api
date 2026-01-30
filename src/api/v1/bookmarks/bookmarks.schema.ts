2409d6b63ad7326e7675de394febfd2e99524f68
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-01-30 15:07:03 +0300

    refactor: define cursorpagination schema

--- src/api/v1/bookmarks/bookmarks.schema.ts
+++ src/api/v1/bookmarks/bookmarks.schema.ts
@@ -1,9 +1,7 @@
 import z from "zod";
 
-export const listBookmarkSchema = z.object({
-    userId: z.uuid(),
-    limit: z.number().int().min(1).max(50).default(5),
-    lastId: z.uuid(),
-})
 
-export type ListBookmarksInput = z.infer<typeof listBookmarkSchema>;
+export const CursorPaginationSchema = z.object({
+    limit: z.number().int().min(1).max(50).default(5),
+    cursor: z.uuid().optional()   
+})
