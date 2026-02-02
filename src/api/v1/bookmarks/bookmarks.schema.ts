883f098935ea010a26b1c5c0328e5a205c9e055f
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-02 14:54:36 +0300

    feat: define BookmarkSchema

--- src/api/v1/bookmarks/bookmarks.schema.ts
+++ src/api/v1/bookmarks/bookmarks.schema.ts
@@ -1,7 +1,19 @@
+import mongoose from "mongoose";
 import z from "zod";
 
+export const BookmarkSchema = z.object({
+    user: z.uuid(),
+    title: z.string(),
+    description: z.string(),
+    url: z.url(),
+    collection: z.uuid(),
+    tags: z.array(z.uuid()),
+    isFavourite: z.boolean()
+})
 
 export const CursorPaginationSchema = z.object({
     limit: z.number().int().min(1).max(50).default(5),
     cursor: z.uuid().optional()   
 })
+
+export type BookmarkInput = z.infer<typeof BookmarkSchema>
