fecc248f16f553a020bb2240b7aaf0bc1dfacaf5
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-02 14:58:54 +0300

    feat: add auth middleware

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from "express";
-import { v1Routers } from "./api/v1/v1.module";
-import { errorMiddleware } from "./middlewares/error.middleware";
 import passport from "passport";
 import "./config/passport";
 
+import { v1Routers } from "./api/v1/v1.module";
+import { errorMiddleware } from "./middlewares/error.middleware";
+import { authMiddleware } from "./middlewares/auth.middleware";
+
 const app = express();
+
 app.use(express.json());
 app.use(passport.initialize());
 
-v1Routers.forEach(r => app.use(`/api/v1${r.basePath}`, r.router));
+v1Routers.forEach(r =>
+  app.use(`/api/v1${r.basePath}`, authMiddleware, r.router)
+);
 
-app.use(errorMiddleware)
+app.use(errorMiddleware);
 
-export default app
+export default app;
