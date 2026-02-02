7aa84e18d43ae58b259ac9031f017a8e148bb892
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-02 14:53:53 +0300

    refactor: add user feild

--- src/models/bookmark.model.ts
+++ src/models/bookmark.model.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 const BookmarkSchema = new mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref:'User', required: true},
     title: {type: String, required: true},
     description: {type: String, required: true},
     url: {type: String, required: true},
