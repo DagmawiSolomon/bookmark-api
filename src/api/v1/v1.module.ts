969c58e0d4a669b87bdf04039a4ff09dbfe0b097
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-06 08:54:49 +0300

    fix:

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -1,3 +1,4 @@
 import { authRouter } from "./auth/auth.module";
+import { bookmarksRouter } from "./bookmarks/bookmarks.route";
 
-export const v1Routers = [authRouter];
+export const v1Routers = [authRouter, bookmarksRouter];
--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -3,24 +3,26 @@ import { ZodError } from "zod";
 import { mapZodError } from "../errors/zod-error";
 import { AppError } from "../errors/app-error";
 
-export const errorMiddleware = (err:Error,res:Response)=>{
-    if(err instanceof ZodError){
+export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof ZodError) {
         const mapped = mapZodError(err)
-        return res.status(mapped.statusCode).json({
+        res.status(mapped.statusCode).json({
             error: mapped.message,
-             details: mapped.details
+            details: mapped.details
         })
+        return
     }
 
-    if(err instanceof AppError){
-        return res.status(err.statusCode).json({
+    if (err instanceof AppError) {
+        res.status(err.statusCode).json({
             error: err.message,
-           
+
         })
+        return
     }
 
     console.error("UNHADLED ERROR: ", err)
-    return res.status(500).json({
+    res.status(500).json({
         error: "Internal Server Error"
     })
 }
