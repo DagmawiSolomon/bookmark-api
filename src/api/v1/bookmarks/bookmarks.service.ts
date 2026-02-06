b54fe79c21ecf8a514f87f998edffd37b81ebc27
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-06 09:40:07 +0300

    refactor: add user id as parameter to create and update Bookmark

--- src/api/v1/bookmarks/bookmarks.service.ts
+++ src/api/v1/bookmarks/bookmarks.service.ts
@@ -1,66 +1,66 @@
 import { InternalServerError } from "../../../errors/http-error"
 import { Bookmark } from "../../../models/bookmark.model";
-import  { BookmarkInput } from "./bookmarks.schema";
+import { BookmarkInput, BookmarkSchema } from "./bookmarks.schema";
 
-const listBookmarks = async (userId: string, limit: number, cursor:string | undefined) => {
-    try{
+const listBookmarks = async (userId: string, limit: number, cursor: string | undefined) => {
+    try {
         const query = cursor
-  ? { _id: { $gt: cursor } }
-  : {};
+            ? { _id: { $gt: cursor } }
+            : {};
 
-        const bookmarks = await Bookmark.find({user:userId, ...query}).sort({_id:1}).limit(limit)
+        const bookmarks = await Bookmark.find({ user: userId, ...query }).sort({ _id: 1 }).limit(limit)
         return bookmarks
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         throw new InternalServerError("Failed to get bookmarks")
     }
 }
 
 const getBookmarkById = async (userId: string, bookmarkId: string) => {
-    try{
-        const bookmark = await Bookmark.find({user:userId, id:bookmarkId})
+    try {
+        const bookmark = await Bookmark.find({ user: userId, id: bookmarkId })
         return bookmark
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         throw new InternalServerError("Failed to get bookmark")
     }
 }
-const createBookmark = async(input:BookmarkInput) => {
-    try{
-        const bookmark = await Bookmark.create(input)
+const createBookmark = async (input: BookmarkInput, userId: string) => {
+    try {
+        const bookmark = await Bookmark.create({ ...input, user: userId })
         return bookmark
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         throw new InternalServerError("Failed to create bookmark")
-    }   
+    }
 
 }
 
 
-const updateBookmark = async(input:BookmarkInput, id: string) => {
-    try{
-        const bookmark = await Bookmark.updateOne({id}, input)
+const updateBookmark = async (input: BookmarkInput, id: string, userId: string) => {
+    try {
+        const bookmark = await Bookmark.updateOne({ _id: id, user: userId }, input)
         return bookmark
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         throw new InternalServerError("Failed to update bookmark")
-    }   
+    }
 }
 
-const deleteBookmark = async(id: string) => {
-    try{
-        const bookmark = await Bookmark.findOneAndDelete({id})
+const deleteBookmark = async (id: string) => {
+    try {
+        const bookmark = await Bookmark.findOneAndDelete({ id })
         return bookmark
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         throw new InternalServerError("Failed to delete bookmark")
-    }   
+    }
 }
 
 
-export const bookmarkServices = {listBookmarks, getBookmarkById, createBookmark, updateBookmark, deleteBookmark}
+export const bookmarkServices = { listBookmarks, getBookmarkById, createBookmark, updateBookmark, deleteBookmark }
