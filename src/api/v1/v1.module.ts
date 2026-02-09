7b5a4794d2969fcb045933ea6dcb9855f81993bd
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-09 17:38:30 +0300

    feat: import tag modules

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -1,4 +1,5 @@
 import { authRouter } from "./auth/auth.module";
 import { bookmarksRouter } from "./bookmarks/bookmarks.route";
 import { collectionRouter } from "./collections/collections.route";
-export const v1Routers = [authRouter, bookmarksRouter, collectionRouter];
+import { tagsRouter } from "./tags/tags.route";
+export const v1Routers = [authRouter, bookmarksRouter, collectionRouter, tagsRouter];
