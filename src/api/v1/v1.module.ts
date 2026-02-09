f8ccfedf9b84f8e7af6c6d03cae748cd67e43061
Dagmawi Solomon <02dagmawisolomon@gmail.com>
2026-02-09 17:48:41 +0300

    feat: import router to module

--- src/api/v1/v1.module.ts
+++ src/api/v1/v1.module.ts
@@ -1,5 +1,6 @@
 import { authRouter } from "./auth/auth.module";
-import { bookmarksRouter } from "./bookmarks/bookmarks.route";
-import { collectionRouter } from "./collections/collections.route";
-import { tagsRouter } from "./tags/tags.route";
+import { bookmarksRouter } from "./bookmarks/bookmarks.module";
+import { collectionRouter } from "./collections/collections.module";
+import { tagsRouter } from "./tags/tags.module";
+
 export const v1Routers = [authRouter, bookmarksRouter, collectionRouter, tagsRouter];
